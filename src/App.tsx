2b0a31420496ca8a6182ad21246182e2a6397f2a
therealtomisin <timilehineribake@yahoo.com>
2022-05-17 16:37:13 +0300

    staging all initial changes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Banner from './Components/Banner';
+
+function App() {
+  return (
+    <div className="app">
+      <Banner />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/axios.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://api.themoviedb.org/3'
+})
+
+// baseURL: 'ba0a3ae0637914b454a10c323e74d13e'
